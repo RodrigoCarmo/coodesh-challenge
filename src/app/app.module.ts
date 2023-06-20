3d9a8a615f783d807ab95bea0ea6a2c0c71108e7
rodrigocarmo <roodrigo_5634@hotmail.com>
2023-06-19 21:20:10 -0300

    chore: configura modulos nest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { dataSourceConfig } from "src/infra/database/typeorm/config/datasource";
 import { ProductsModule } from "./modules/products.module";
+import { ScheduleModule } from "@nestjs/schedule";
+import { JobService } from "./services/job.service";
+import { ProductsRepository } from "src/infra/database/typeorm/repositories/products.repository";
+import { OpenFoodFactService } from "src/infra/http/services/openfoodfacts.service";
+import { HttpModule } from "@nestjs/axios";
+import { FilesManagerRepository } from "src/infra/database/typeorm/repositories/files-manager.repository";
 
 @Module({
   imports: [
+    HttpModule,
+    ScheduleModule.forRoot(),
     ProductsModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) =>
@@ -16,6 +24,11 @@ import { ProductsModule } from "./modules/products.module";
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    JobService,
+    { provide: "PRODUCT_REPOSITORY", useClass: ProductsRepository },
+    { provide: "OPEN_FOOD_FACT_SERVICE", useClass: OpenFoodFactService },
+    { provide: "FILES_MANAGER_REPOSITORY", useClass: FilesManagerRepository },
+  ],
 })
 export class AppModule {}
--- src/app/modules/products.module.ts
+++ src/app/modules/products.module.ts
@@ -4,13 +4,22 @@ import { ProductsController } from "../controllers/products.controller";
 import { ProductsService } from "../services/product.service";
 import { ProductEntity } from "src/infra/database/typeorm/entities/product.entity";
 import { ProductsRepository } from "src/infra/database/typeorm/repositories/products.repository";
+import { OpenFoodFactService } from "src/infra/http/services/openfoodfacts.service";
+import { HttpModule } from "@nestjs/axios";
+import { FilesManagerRepository } from "src/infra/database/typeorm/repositories/files-manager.repository";
+import { FilesManagerEntity } from "src/infra/database/typeorm/entities/files.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity, FilesManagerEntity]),
+    HttpModule,
+  ],
   controllers: [ProductsController],
   providers: [
     ProductsService,
     { provide: "PRODUCT_REPOSITORY", useClass: ProductsRepository },
+    { provide: "OPEN_FOOD_FACT_SERVICE", useClass: OpenFoodFactService },
+    { provide: "FILES_MANAGER_REPOSITORY", useClass: FilesManagerRepository },
   ],
   exports: [TypeOrmModule],
 })
