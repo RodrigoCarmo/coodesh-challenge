a7838318a785eceaca08c40f32e197c3dd1df9fb
rodrigocarmo <roodrigo_5634@hotmail.com>
2023-06-21 09:34:55 -0300

    feat: service para produtos

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,11 @@
 import { HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
 import { ProductModel } from "src/domain/models/product.model";
 import { ProductsRepositoryInterface } from "src/domain/repositories/interfaces/products.repository.interface";
+import {
+  GetByCodeDto,
+  GetProductsDto,
+  UpdateProductDto,
+} from "../dto/products.dto";
 
 @Injectable()
 export class ProductsService {
@@ -16,5 +21,26 @@ export class ProductsService {
       throw new HttpException("Product not found", HttpStatus.NOT_FOUND);
 
     return product;
+  }
+
+  async getProducts(getProductsDto: GetProductsDto): Promise<ProductModel[]> {
+    return this.productsRepository.getProducts(
+      Number(getProductsDto.skip) || 1,
+      Number(getProductsDto.take) || 10
+    );
+  }
+
+  async updateProduct(
+    getByCodeDto: GetByCodeDto,
+    updateProductDto: UpdateProductDto
+  ): Promise<void> {
+    return this.productsRepository.updateProduct(
+      getByCodeDto,
+      updateProductDto
+    );
+  }
+
+  async deleteProduct(getByCodeDto: GetByCodeDto): Promise<void> {
+    return this.productsRepository.deleteProduct(getByCodeDto.code);
   }
 }
